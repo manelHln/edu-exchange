626f49b4967552d02f382da5209bf2467bf50340
Lordis Adjele <lordis.adjele@gmail.com>
2023-10-04 15:15:24 +0100

    project init

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# EDU-EXCHANGE PROJECT
+
+* Start project
+1. Clone repo
+2. cd front-side/
+3. npm install
+4. cd ..
+5. docker-compose build
+6. docker-compose up -d
---
+++ eduExchange/back-api/src/main/java/org/project/backapi/BackApiApplication.java
@@ -0,0 +1,13 @@
+package org.project.backapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackApiApplication.class, args);
+	}
+
+}

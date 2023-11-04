abda1191251dbeacdc50f10c3e6da10e80c4cec5
Emmanuel HOLONOU <manelhln00@gmail.com>
2023-11-04 18:46:33 +0100

    Added dark mode and light mode

--- eduExchange/front-side/src/pages/_app.js
+++ eduExchange/front-side/src/pages/_app.js
@@ -3,12 +3,20 @@ import Router from "next/router";
 import LoadingEffect from "@/components/LoadingEffect";
 import "@/styles/globals.css";
 import Layout from "@/components/layout";
+import { ThemeProvider } from "@/components/theme-provider";
 
 export default function App({ Component, pageProps }) {
-  Router.events.on("routeChangeStart", ()=> <LoadingEffect />)
+  Router.events.on("routeChangeStart", () => <LoadingEffect />);
   return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="light"
+      enableSystem
+      disableTransitionOnChange
+    >
       <Layout>
         <Component {...pageProps} />
       </Layout>
+    </ThemeProvider>
   );
 }

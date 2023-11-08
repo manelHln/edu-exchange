4770b25660b2a2493f3e6332afafc493e25e36dc
Lordis Adjele <lordis.adjele@gmail.com>
2023-11-08 13:29:52 +0100

    messages and conversations logic : remains message update and delete

--- eduExchange/back-api/src/main/java/org/project/backapi/exception/RestResponseEntityExceptionHandler.java
+++ eduExchange/back-api/src/main/java/org/project/backapi/exception/RestResponseEntityExceptionHandler.java
@@ -1,11 +1,13 @@
 package org.project.backapi.exception;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+@ControllerAdvice
 public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(value = RessourceNotFoundException.class)
